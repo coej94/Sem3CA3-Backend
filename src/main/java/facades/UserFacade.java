7012140c2ed7894e7e74a74a5816e3706d976b63
Staalet <staalthomas1@gmail.com>
2017-04-04 11:51:53 +0200

    fixed UserFacade method authenticateUser to check if the given password is the same as in the database

--- src/main/java/facades/UserFacade.java
+++ src/main/java/facades/UserFacade.java
@@ -3,51 +3,78 @@ package facades;
 import security.IUserFacade;
 import entity.User;
 import java.util.List;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
 import security.IUser;
 import security.PasswordStorage;
 
 public class UserFacade implements IUserFacade {
 
-  EntityManagerFactory emf;
+    EntityManagerFactory emf;
 
-  public UserFacade(EntityManagerFactory emf) {
-    this.emf = emf;   
-  }
-
-  private EntityManager getEntityManager() {
-    return emf.createEntityManager();
-  }
-
-  @Override
-  public IUser getUserByUserId(String id) {
-    EntityManager em = getEntityManager();
-    try {
-      return em.find(User.class, id);
-    } finally {
-      em.close();
+    public UserFacade(EntityManagerFactory emf) {
+        this.emf = emf;
     }
-  }
 
-  /*
+    private EntityManager getEntityManager() {
+        return emf.createEntityManager();
+    }
+
+    @Override
+    public IUser getUserByUserId(String id) {
+        EntityManager em = getEntityManager();
+        try {
+            return em.find(User.class, id);
+        } finally {
+            em.close();
+        }
+    }
+
+    /*
   Return the Roles if users could be authenticated, otherwise null
-   */
-  @Override
-  public List<String> authenticateUser(String userName, String password) {
-    IUser user = getUserByUserId(userName);
-    return user != null && password.equals(user.getPassword()) ? user.getRolesAsStrings() : null;  
-  }
+     */
+    @Override
+    public List<String> authenticateUser(String userName, String password) {
+        IUser user = getUserByUserId(userName);
+        System.out.println(user.getPassword());
+        if (user != null) {
+            try {
+                //if (password.equals(user.getPassword())) {
+                if (PasswordStorage.verifyPassword(password, user.getPassword())) {
+                    System.out.println(user.getRolesAsStrings());
+                    return user.getRolesAsStrings();
+                    
+                }
+                
+            } catch (PasswordStorage.CannotPerformOperationException ex) {
+                System.out.println("cannot perfom operation exception authenticateUser 1");
+            } catch (PasswordStorage.InvalidHashException ex) {
+                System.out.println("cannot perfom operation exception authenticateUser 2");
+            }
+        } else {
+            return null;
+        }
+        return null;
+    }
 
-  public void createUser(String userName, String password) throws PasswordStorage.CannotPerformOperationException{
-     EntityManager em = emf.createEntityManager();
-     User user = new User(userName, password);
-     try {
-         em.getTransaction().begin();
-         em.persist(user);
-         em.getTransaction().commit();
-     } finally {
-         em.close();
-     }
-  }
+    public void createUser(String userName, String password) throws PasswordStorage.CannotPerformOperationException {
+        EntityManager em = emf.createEntityManager();
+        User user = new User(userName, password);
+        try {
+            em.getTransaction().begin();
+            em.persist(user);
+            em.getTransaction().commit();
+        } finally {
+            em.close();
+        }
+    }
+
+    public static void main(String[] args) {
+        UserFacade uf = new UserFacade(Persistence.createEntityManagerFactory("pu_development", null));
+        List<String> hej = uf.authenticateUser("user", "test");
+        System.out.println(hej);
+    }
 }
