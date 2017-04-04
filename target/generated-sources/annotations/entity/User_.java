de59028badbeec418bdeaa260b6cdee4b74a585e
coej94 <coej94@hotmail.com>
2017-04-04 12:02:28 +0200

    merge conflict

--- target/generated-sources/annotations/entity/User_.java
+++ target/generated-sources/annotations/entity/User_.java
@@ -6,7 +6,7 @@ import javax.persistence.metamodel.ListAttribute;
 import javax.persistence.metamodel.SingularAttribute;
 import javax.persistence.metamodel.StaticMetamodel;
 
-@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-03T15:45:33")
+@Generated(value="EclipseLink-2.5.2.v20140319-rNA", date="2017-04-04T11:08:05")
 @StaticMetamodel(User.class)
 public class User_ { 
 
